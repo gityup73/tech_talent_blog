3095b520c734e8422cdaf727ef5e33154a4f0f9a
Jessica Swirat <soxfan1173@gmail.com>
2016-10-12 10:23:05 -0400

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,49 @@
+class CommentsController < ApplicationController
+
+  before_action :set_comment_post, only: [:edit, :update, :destroy]
+
+  def edit
+  end
+
+  def create
+  	@comment = Comment.new(comment_params)
+
+  	respond_to do |format|
+  		if @comment.save
+  			format.html { redirect_to blog_post_path(id: @comment.blog_post_id), notice: "Comment was created successfully"}
+  		else
+  			format.html { redirect_to :edit}
+  		end
+  	end
+  end
+
+  def update
+  	respond_to do |format|
+  		if @comment.update(comment_params)
+  			format.html { redirect_to blog_post_path(id: @comment.blog_post_id), notice: "Comment was updated successfully"}
+  		else
+  			format.html { redirect_to :edit}
+  		end
+
+  	end
+  end
+
+  def destroy
+  	@comment.destroy
+
+  	respond_to do |format|
+  		format.html {redirect_to blog_post_path(id: @comment.blog_post_id), notice: "Your comment was destroyed!"}
+  	end
+
+  end
+
+  private 
+  def set_comment_post
+  	@comment = Commment.find(params[:id])
+  end
+
+  def comment_params
+  	params.require(:comment).permit(:author, :comment_entry, :blog_post_id)
+  end
+end
+
